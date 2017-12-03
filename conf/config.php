c4c85f7c088e42daa8f1ee9ddd596b55a3afcbc4
Véronique Wuyts <s0008028@student.thomasmore.be>
2017-12-03 15:43:09 +0100

    Update: config.ini changed into config.php

---
+++ conf/config.php
@@ -0,0 +1,23 @@
+<?php
+/* Lego Webshop
+ *
+ * Lab assignment for course PHP & MySQL 2017
+ * Thomas More campus De Nayer
+ * Bachelor Elektronica-ICT -- Application Development
+ * 
+ * Véronique Wuyts
+ * 
+ * Configuration file
+ */
+
+return array(
+    'host'          => 'localhost',
+    'port'          => 3306,
+    'user'          => 'Webgebruiker',
+    'passw'         => 'Labo2017',
+    'db'            => 'Legoshop',
+    'activityLog'   => 'C:/xampp/htdocs/PHP&MySQL/Lego_webshop/log/activityLog',
+    'errorLog'      => 'C:/xampp/htdocs/PHP&MySQL/Lego_webshop/log/errorLog',
+    'maxLogFiles'   => 10
+);
+?>
